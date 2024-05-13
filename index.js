101c1adafbb644a66b9cb930d4bffce691588130
redrumkumg <n.ronnapat@github.com>
2024-05-13 14:23:38 +0700

    apply ghost bear

---
+++ index.js
@@ -0,0 +1,13 @@
+let Products = {
+    product_id: 1,
+    product_name: "ghost bear",
+    quantity_in_stock: 19,
+    unit_price: 300,
+    product_description: {
+        product_brand: "shinwoo",
+        size_or_quantity: "small",
+        taste_or_scent: "grape",
+        color: "red",
+        additional_description: "Arts toy"
+    }
+}
