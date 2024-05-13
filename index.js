08f1334c9c51dfd45eb767cf6c907fe6700b893b
redrumkumg <n.ronnapat@github.com>
2024-05-13 14:29:09 +0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let Products = {
-    product_id: 1,
-    product_name: "ghost bear",
-    quantity_in_stock: 19,
-    unit_price: 300,
-    product_description: {
-        product_brand: "shinwoo",
-        size_or_quantity: "small",
-        taste_or_scent: "grape",
-        color: "red",
-        additional_description: "Arts toy"
-    }
-}
+const Products = {
+  product_id: 1,
+  product_name: "ghost bear",
+  quantity_in_stock: 19,
+  unit_price: 300,
+  product_description: {
+    product_brand: "shinwoo",
+    size_or_quantity: ["small","large"],
+    taste_or_scent: ["grape","melon","orange"],
+    color: ["red", "green", "blue"],
+    additional_description: "Arts toy",
+  },
+};
+
+Products.product_description.product_brand = "test"
+
+console.log(Products);
